e9a8e097065d6cf07420eb6259365d3ff8295c2e
pablovirtual <143946760+pablovirtual@users.noreply.github.com>
2025-03-09 17:44:39 -0600

    configuracion para comunicacion con angular

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Configurar el middleware CORS usando el middleware integrado
+        $middleware->use([HandleCors::class]);
+        
+        // También puedes configurar el grupo de api si necesitas
+        // $middleware->api();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
